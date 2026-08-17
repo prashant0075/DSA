0c4fed413442fae8c94c950621211fd255bafbfb
Prashant Uikey <141541107+prashant0075@users.noreply.github.com>
2026-08-17 21:36:51 +0530

    Time: 8 ms (9.99%) | Memory: 46.5 MB (94.10%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode temp = head;
+        Map<ListNode,Integer> mpp = new HashMap<>();
+        while(temp != null){
+            if(mpp.containsKey(temp)){
+                return true;
+            }
+            else{
+                mpp.put(temp,1);
+            }
+            temp = temp.next;
+        }
+    return false;
+    }
+}
