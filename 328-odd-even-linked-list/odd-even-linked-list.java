d2fd4cbeb74a4db54182dddc95cd53d7a2880638
Prashant Uikey <141541107+prashant0075@users.noreply.github.com>
2026-08-20 17:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (95.27%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,32 +10,23 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        int t = 1;
-        if(head == null || head.next == null) return head;
-        while(temp != null && temp.next != null){
-            arr.add(temp.val);
-            temp = temp.next.next;
+        if(head == null || head.next== null){
+            return head;
         }
-        if(temp != null){
-            arr.add(temp.val);
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        if(head == null || head.next== null){
+            return head;
         }
-        temp = head.next;
-        while(temp != null&&temp.next != null){
-            arr.add(temp.val);
-            temp = temp.next.next;
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next; 
         }
-        if(temp != null){
-            arr.add(temp.val);
-        }
-        int i = 0;
-        temp = head;
-        while(temp != null){
-            temp.val = arr.get(i);
-            temp = temp.next;
-            i++;
-        }
+        odd.next = evenHead;
         return head;
     }
 }
